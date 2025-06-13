c89b265012b7c3f09b14afc53fc86003138c4473
ningshunqiang <920062955@qq.com>
2025-06-13 17:40:39 +0800

    fix: 流水线修改

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import Button from './components/button';
 // import './index.css'
 // import App from './App.tsx'
 
-
-
 // export { default as Button } from './components/button';
 // export { default as Image } from './components/image';
 createRoot(document.getElementById('root')!).render(
