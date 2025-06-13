daf8bf11e6ca83451b646b41b875c2d42cbd2d47
ningshunqiang <920062955@qq.com>
2025-06-13 16:48:06 +0800

    流水线去除测试

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import Button from './components/button';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Button from './components/button';
 // import './index.css'
 // import App from './App.tsx'
 
 
 
-export { default as Button } from './components/button';
-export { default as Image } from './components/image';
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <Button children='111' />
-//   </StrictMode>,
-// )
+// export { default as Button } from './components/button';
+// export { default as Image } from './components/image';
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Button children='111' />
+  </StrictMode>,
+)
 
 
